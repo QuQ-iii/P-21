fae1f70693d576704982eeee2ff71fa9522e7053
QuQ-iii <75155537+QuQ-iii@users.noreply.github.com>
2020-12-09 23:10:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var bullet, wall;
+var speed, weight, thickness;
+
+function setup() {
+  createCanvas(800,400);
+
+  speed = random(223,321);
+  weight = random(30,52);
+  thickness = random(22,83);
+
+  wall = createSprite(650,200,thickness,200);
+ 
+  bullet = createSprite(20,200,20,5);
+  
+
+  bullet.velocityX = speed;
+
+}
+
+function draw() {
+  background("black");  
+
+  if(isTouching(bullet,wall)){
+
+     var damage = 0.5 * weight * speed * speed / (thickness * thickness * thickness);
+
+     bullet.velocityX = 0;
+
+     if(damage > 10){
+       wall.shapeColor = color(255, 0, 0);
+     }else{
+        wall.shapeColor = color(0, 255, 0);
+      }
+  }
+
+  drawSprites();
+}
